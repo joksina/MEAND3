8b07b56c6586762754a9bf0dcaf7c55282b021da
joksina <jksalaudeen@yahoo.com>
2016-01-19 12:26:02 -0800

    created a form to keep adding words

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var app = angular.module("news", []);
 
 app.controller("MainController", ['$scope', function ($scope){
-    $scope.test = "hello";
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}];
+  $scope.test = "hello";
+  $scope.posts = [
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 15},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 4}];
+
+  //A function to append a post to the innerHTML
+  $scope.addPost = function () {
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
 }]);
