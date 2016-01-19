a17536b5cb0c414f18894501e3410d4a9d59f860
joksina <jksalaudeen@yahoo.com>
2016-01-19 12:12:08 -0800

    using ng-repeat to iterate through an array of object

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ var app = angular.module("news", []);
 
 app.controller("MainController", ['$scope', function ($scope){
     $scope.test = "hello";
-    $scope.posts = ['post 1', 'post 2', 'post 3', 'post 4'];
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}];
 }]);
