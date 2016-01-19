dcf526398a2095c48d488e2712b3e6465ca9f778
joksina <jksalaudeen@yahoo.com>
2016-01-19 11:54:15 -0800

    using ng-repeat that iterates through a list of posts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var app = angular.module("news", []);
 
-app.controller("MainController", function ($scope){
+app.controller("MainController", ['$scope', function ($scope){
     $scope.test = "hello";
-});
+    $scope.posts = ['post 1', 'post 2', 'post 3', 'post 4'];
+}]);
