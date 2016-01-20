2a32231d2ac006d70ff45432b3a39d3870e8fb15
joksina <jksalaudeen@yahoo.com>
2016-01-19 21:03:17 -0800

    using ui views and inline templates

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-var app = angular.module("news", []);
+// when the controller goes out of scope, we lose the data
+// that data cannot be easily accessed from other controllers or directives
+// the data is difficult to mock, which is important when writing automated tests
+//using ui-routes because it's new and mor flexible
+var app = angular.module("news", ['ui.router']);
 
-app.controller("MainController", ['$scope', function ($scope){
+app.factory('posts', [function () {
+  var o = {
+    posts: []
+  };
+
+  return o;
+}]);
+
+app.controller("MainController", ['$scope', 'posts', function ($scope, posts){
   $scope.test = "hello";
+  $scope.posts = posts.posts;
   $scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
@@ -11,15 +24,45 @@ app.controller("MainController", ['$scope', function ($scope){
 
   //A function to append a post to the innerHTML
   $scope.addPost = function () {
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0,
+      comments: [
+        {author: 'AJ', body: 'Cool post!', upvotes: 0},
+        {author: 'Lyly', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
+    });
     $scope.title = '';
+    $scope.link = '';
     //prevent user from submiting a blank title
     if(!$scope.title || $scope.title === '') {
       return;
     }
   };
   //create a function that increments the upvotes
+  //post is being passed to reference the post in the html file
   $scope.incrementUpvotes = function (post) {
-    post.upvotes += 1
-  }
+    post.upvotes += 1;
+  };
 }]);
+
+app.controller('PostController', ['$scope', '$stateParams', 'posts'], function ($scope, $stateParams, posts) {
+  $scope.post = posts.posts[$stateParams.id];
+});
+
+//configure the app to route to url
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainController'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostController'
+    });
+    $urlRouterProvider.otherwise('home');
+}])
